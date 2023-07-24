669d286cb9fc82b5e9c4e1daa6c6b5803855e748
Mohammed Alrefai <mohammed@refam.io>
2023-07-25 00:09:31 +0300

    refactor app with clsx and tailwind-merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,57 @@
 import { useState } from 'react'
 
-function App() {
+import { cn } from './lib/utils.ts'
+
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const hearderProps = {
+    className: cn([
+      `min-h-100vh`,
+      `flex-(~ col)`,
+      `items-center justify-center`,
+      `bg-#282c34 color-white`,
+    ]),
+  } satisfies React.HTMLAttributes<HTMLElement>
+
+  const buttonProps = {
+    type: `button`,
+    className: cn([
+      `p-(y2 x-2)`,
+      `bg-blue-400 dark:bg-blue-500`,
+      `hover:(bg-blue-500 dark:bg-blue-600)`,
+      `text-(sm dark)`,
+      `font-(mono light)`,
+      `border-(2 blue-200)`,
+      `rounded`,
+    ]),
+    onClick: () => setCount(count => count + 1),
+  } satisfies React.ButtonHTMLAttributes<HTMLButtonElement>
+
+  const anchorProps = {
+    target: `_blank`,
+    rel: `noopener noreferrer`,
+    className: `color-#61dafb`,
+  } satisfies React.AnchorHTMLAttributes<HTMLAnchorElement>
 
   return (
     <div className='text-center'>
-      <header className='min-h-100vh flex flex-col items-center justify-center bg-#282c34 color-white'>
+      <header {...hearderProps}>
         <div className='logo' />
-        <h1 className='mt-2em animate-bounce-alt animate-duration-2s'>
+        <h1 mt='2em' animate='bounce-alt duration-2s'>
           Hello Vite + React!
         </h1>
         <p>
-          <button
-            className='border-2 border-blue-200 rounded bg-blue-400 px-4 py-2 text-sm font-light font-mono text-white dark:bg-blue-500 hover:bg-blue-500 dark:hover:bg-blue-600'
-            type='button'
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-
-          <button
-            bg='blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600'
-            text='sm white'
-            font='mono light'
-            p='y-2 x-4'
-            m='l-1em'
-            border='2 rounded blue-200'
-            type='button'
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
+          <button {...buttonProps}>count is: {count}</button>
+          <button {...buttonProps} ml='1em'>count is: {count}</button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className='color-#61dafb'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
+          <a {...anchorProps} href='https://reactjs.org'>Learn React</a>
           {` | `}
-          <a
-            className='color-#61dafb'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
+          <a {...anchorProps} href='https://vitejs.dev/guide/features.html'>
             Vite Docs
           </a>
         </p>
